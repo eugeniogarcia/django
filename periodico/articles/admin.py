6e8677ba7a089db082908aaabbfc276f4d6e7627
eugenio <egsmartin@gmail.com>
2022-03-29 12:57:53 +0200

    .

--- periodico/articles/admin.py
+++ periodico/articles/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
-admin.site.register(Article)
+
+class CommentInline(admin.TabularInline):  # new
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+#Registramos el modelo Article, e indicamos que se use un módulo administrativo que hemos tuneado para incluir los inlines
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
